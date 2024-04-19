90b1ff36a7ad6bb3f7236cfb683a7735ebec5956
Amna <ammy5342@gmail.com>
2024-04-19 21:15:19 +0200

    Added script for checking last quest

--- home/kitchen/pantry/.back_of_the_pantry/check.sh
+++ home/kitchen/pantry/.back_of_the_pantry/check.sh
@@ -1,4 +1,45 @@
-# check if there is a box containing 1 kahk in .back_of_the_pantry where the rat is
-# and check if there is kahk_box in the living room containing 19 kahk
-# if so, print "Congratulations! Look for the ending hidden in this folder"
-# Else print an error message depending on the failing condition
+#!/bin/bash
+#!/bin/bash
+cleared=true
+echo "-----------------------------------------------------------------"
+
+if [[ ! -d "./rat_kahk_box" ]]; then
+    cleared=false
+    echo "Make a box (rat_kahk_box) for the rat and put 1 kahk there."
+else
+    rat_kahks=$(ls "./rat_kahk_box" | wc -l)
+    if [[ $rat_kahks -lt 1 ]]; then
+        cleared=false
+        echo "Give the poor rat a kahk!"
+    elif [[ $rat_kahks -gt 1 ]]; then
+        echo "$rat_kahks kahks for the rat?"
+        echo "You're not this gracious. Take one back!"
+        echo ""
+        cleared=false
+    fi
+fi
+
+if [[ ! -d "../../../living_room/kahk_box" ]]; then
+    cleared=false
+    echo "Take your box (kahk_box) back to the living room."
+    #condition 4
+else
+    kahks=$(ls "../../../living_room/kahk_box" | wc -l)
+
+    if [[ $kahks -lt 19 ]]; then
+        cleared=false
+        echo "You're supposed to have 19 kahks..."
+        echo ""
+
+    fi
+fi
+
+if [[ $cleared == false ]]; then
+    echo "Finish the remaining tasks and try again..."
+    return
+fi
+echo "Well done!"
+cat "../../../.assets/.rat_thank_you"
+echo ""
+read -p "Ready to enjoy your kahk? [Press any key to continue]" $choice
+bash "../../../.assets/.ending_script.sh"
