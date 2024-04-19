0f987d0dd7f4bbf4c0fab696d77c7b34b5bdfc1e
Amna <ammy5342@gmail.com>
2024-04-19 22:31:58 +0200

    Added extra hint in pantry task

--- home/kitchen/pantry/check.sh
+++ home/kitchen/pantry/check.sh
@@ -15,8 +15,10 @@ done
 if [[ $cleared == "no" ]]; then
   echo "Throw all expired items and try again..."
 else
-  echo "Well done! You can now eat worry-free.."
-
+  echo "Well done! Your pantry is a lot tidier now..."
+  echo "Wait a second..."
+  echo "You can now see something in the back, but its hidden from your sight!"
+  echo "(Hint: use ls with a certain option!)"
 fi
 
 echo "-----------------------------------------------------------------"
